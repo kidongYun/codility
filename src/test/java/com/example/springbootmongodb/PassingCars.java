0a1d14bd336cee1c1e7c0cb35d56dddefaa5d674
Kidong Yun <oriondigestive@gmail.com>
2021-02-26 18:19:17 +0900

    public int solution(int[] A) {

--- src/test/java/com/example/springbootmongodb/PassingCars.java
+++ src/test/java/com/example/springbootmongodb/PassingCars.java
@@ -1,24 +1,6 @@
 package com.example.springbootmongodb;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.*;
-
-import static java.util.stream.Collectors.toList;
-
-
-@SpringBootTest
-class SpringbootmongodbApplicationTests {
-
-    @Test
-    void contextLoads() {
-        int[] A = {0, 1, 0, 1, 1};
-
-        int result = solution(A);
-        System.out.println(result);
-    }
-
+public class PassingCars {
     public int solution(int[] A) {
         if (A.length == 1) return 0;
 
