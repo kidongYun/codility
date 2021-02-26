337aa4325d7861821a7e27849bd6e3d261249ee6
Kidong Yun <oriondigestive@gmail.com>
2021-02-26 17:53:28 +0900

    PermCheck

--- src/test/java/com/example/springbootmongodb/PermCheck.java
+++ src/test/java/com/example/springbootmongodb/PermCheck.java
@@ -1,24 +1,8 @@
 package com.example.springbootmongodb;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Arrays;
 
-import java.util.*;
-
-import static java.util.stream.Collectors.toList;
-
-
-@SpringBootTest
-class SpringbootmongodbApplicationTests {
-
-    @Test
-    void contextLoads() {
-        int[] A = {4, 1, 3};
-
-        int result = solution(A);
-        System.out.println(result);
-    }
-
+public class PermCheck {
     public int solution(int[] A) {
         if(A.length == 0) return 0;
         if(A.length == 1) {
